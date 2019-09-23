0045a22454392b02d535219f732eee757179afc4
datomegasherly <dat.omega.sherly@gmail.com>
2019-09-23 23:21:11 +0330

    create eloquent model for admin_users
    this eloquent will set a connection to admin_users table to check if
    user is login or not and fetch data from admin_users table

---
+++ app/AdminUsers.php
@@ -0,0 +1,11 @@
+<?php
+/** php artisan make:model Admin @by: @MAGIC 20190922 */
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AdminUsers extends Model
+{
+    /** current table is admin_users @by: @MAGIC 20190923 */
+    protected $table = 'admin_users';
+}
