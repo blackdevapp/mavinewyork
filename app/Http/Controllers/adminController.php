e261a44fa6ca158f1f8a85d7f8336f5fc47adb06
datomegasherly <dat.omega.sherly@gmail.com>
2019-09-24 00:16:39 +0330

    add index and showLogin functions to adminController
    add index and showLogin functions to adminController

--- app/Http/Controllers/adminController.php
+++ app/Http/Controllers/adminController.php
@@ -8,7 +8,7 @@ class adminController extends Controller
 {
     public function index(){
         /** check if admin is login or not , then refer to current page */
-        echo 'ss';
+        echo 'is index';
     }
     public function showLogin(){
         return view('admin.login');
